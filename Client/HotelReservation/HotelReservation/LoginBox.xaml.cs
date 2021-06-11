3f3aee6f712ce944b9ce94c6a6ba37bc30fd55e7
isiemaszko <izabela.siemaszko@onet.pl>
2021-06-11 23:04:53 +0200

    server - basic auth

---
+++ Client/HotelReservation/HotelReservation/LoginBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace HotelReservation
+{
+    public partial class LoginBox : Window
+    {
+        public LoginBox()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UsernameBox.Text.Length > 0 && PasswordBox.Password.Length > 0)
+            {
+                AsyncTaskMessage.Foreground = Brushes.Black;
+                AsyncTaskMessage.Text = "Logging in";
+                if (await ServiceConnection.GetConnection().Login(UsernameBox.Text, PasswordBox.Password))
+                {
+                    AsyncTaskMessage.Text = "";
+                    new MainWindow().Show();
+                    Close();
+                }
+                AsyncTaskMessage.Text = "Invalid username or password";
+            }
+            else
+            {
+                AsyncTaskMessage.Text = "Fill username and password";
+            }
+            AsyncTaskMessage.Foreground = Brushes.Red;
+        }
+
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+                LoginButton_Click(sender, e);
+        }
+    }
+}
---
+++ Client/HotelReservation/HotelReservation/OccupancyConverter.cs
@@ -0,0 +1,23 @@
+﻿using HotelReservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace HotelReservation
+{
+    public class OccupancyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Room room = value as Room;
+            return (room.hasDoubleBed ? 2 : 0) + room.numberOfSingleBeds;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
